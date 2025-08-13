1fffd790761b11817a31f760fe709c5def15414d
Alex Romero Prieto <nasridexp@gmail.com>
2025-08-13 09:20:51 +0100

    Update fractional release data for consistency

--- tools/calculating_fractional_release.py
+++ tools/calculating_fractional_release.py
@@ -1,10 +1,10 @@
 # Calculate the fractional release of a species given its ozone depleting power (ODP)
 
 # Data for target species
-odp = 1.56
-total_lifetime = 6.6 # 1
-molecule_mass = 197.4 # g/mol
-number_br_atoms = 1
+odp = 0.03
+total_lifetime = 5.9 # 1
+molecule_mass = 184.5 # g/mol
+number_br_atoms = 0
 number_cl_atoms = 1
 number_io_atoms = 0
 
@@ -13,7 +13,7 @@ fractional_release_CFC_11 = 0.47
 total_lifetime_CFC_11 = 52 # years
 molecule_mass_CFC_11 = 137.36
 
-rel_factor_br = 60
+rel_factor_br = 50
 rel_factor_io = 240
 
 new_fractional_release = 3 * (fractional_release_CFC_11 / (number_cl_atoms + number_br_atoms*rel_factor_br + number_io_atoms*rel_factor_io)) * odp * (total_lifetime_CFC_11/total_lifetime) * (molecule_mass/molecule_mass_CFC_11)
